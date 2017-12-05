9c4acb48928fdf847fd9f082c2bd12ec8b6ba441
Diana Man <diana.man.91@gmail.com>
2017-12-05 18:00:29 -0300

    select id

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { CardSection } from './common';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+
 	render() {
 		return (
-			<CardSection>
-				<Text>{this.props.library.title}</Text>
-			</CardSection>
+			<TouchableWithoutFeedback
+				onPress={() => this.props.selectLibrary(this.props.library.id)}
+			>
+				<View>
+					<CardSection>
+						<Text style={styles.titleStyle}>{this.props.library.title}</Text>
+					</CardSection>
+				</View>
+			</TouchableWithoutFeedback>
 		);
 	}
 }
 
-export default ListItem;
+const styles = {
+	titleStyle: {
+		fontSize: 18,
+		paddingLeft: 15
+	}
+}
+
+export default connect(null, actions)(ListItem);
